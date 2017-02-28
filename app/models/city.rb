4b3f08dae7a6cd800dacf85d4a908644213e9e0d
Raghu Betina <raghu@firstdraft.com>
2017-02-28 01:11:42 +0000

    Direct association between cities and follows

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,9 @@
 class City < ApplicationRecord
   # Direct associations
 
+  has_many   :follows,
+             :dependent => :destroy
+
   has_many   :recipes,
              :dependent => :destroy
 
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,8 @@
 class Follow < ApplicationRecord
   # Direct associations
 
+  belongs_to :city
+
   belongs_to :user
 
   # Indirect associations
