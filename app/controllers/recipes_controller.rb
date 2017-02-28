2630670bc7807727c902975dadde0b7ba6eadded
Raghu Betina <raghu@firstdraft.com>
2017-02-28 01:12:28 +0000

    Only recipe user can modify recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,14 @@
 class RecipesController < ApplicationController
+  before_action :current_user_must_be_recipe_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_recipe_user
+    recipe = Recipe.find(params[:id])
+
+    unless current_user == recipe.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @recipes = Recipe.all
 
