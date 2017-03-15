bf0337853e7caa7b83a7cac9ee5f2458b0e99825
Raghu Betina <raghu@firstdraft.com>
2017-03-15 16:15:41 +0000

    4th draft

--- app/admin/city.rb
+++ app/admin/city.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register City do
 
- permit_params :country_id, :image, :description
+ permit_params :country_id, :image, :description, :name
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,9 @@
 class Country < ApplicationRecord
   # Direct associations
 
+  has_many   :recipes,
+             :dependent => :destroy
+
   has_many   :cities,
              :dependent => :destroy
 
