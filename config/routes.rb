d6d83b6ad893f855790a869d58e38d0b0c863eaf
tilbec <tilbec@users.noreply.github.com>
2017-03-16 14:07:57 -0500

    Thursday 2:07pm

--- config/routes.rb
+++ config/routes.rb
@@ -38,19 +38,19 @@ Rails.application.routes.draw do
 
   # Routes for the Country resource:
   # CREATE
-  get "/countries/new", :controller => "countries", :action => "new"
-  post "/create_country", :controller => "countries", :action => "create"
+  #get "/countries/new", :controller => "countries", :action => "new"
+  #post "/create_country", :controller => "countries", :action => "create"
 
   # READ
-  get "/countries", :controller => "countries", :action => "index"
+  #get "/countries", :controller => "countries", :action => "index"
   get "/countries/:id", :controller => "countries", :action => "show"
 
   # UPDATE
-  get "/countries/:id/edit", :controller => "countries", :action => "edit"
-  post "/update_country/:id", :controller => "countries", :action => "update"
+  #get "/countries/:id/edit", :controller => "countries", :action => "edit"
+  #post "/update_country/:id", :controller => "countries", :action => "update"
 
   # DELETE
-  get "/delete_country/:id", :controller => "countries", :action => "destroy"
+  #get "/delete_country/:id", :controller => "countries", :action => "destroy"
   #------------------------------
 
   # Routes for the Recipe resource:
